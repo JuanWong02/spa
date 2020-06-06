ee9f1484b8a09e84b8e644cd3a711f2b1674a88e
JuanWong02 <juanwong0211@gmail.com>
2020-06-06 14:09:58 -0600

    spa terminado

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <p>Hello Juan</p>
+
+      <ul>
+          <li>Elemento 1</li>
+          <li>Elemento 2</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import './../css/style.css';
+export default {
+
+}
+</script>
+
+<style>
+    p {
+        color: red;
+    }
+</style>
---
+++ src/index.js
@@ -0,0 +1,14 @@
+//require de Node.js
+//import de Javascript
+import Vue from 'vue';
+
+import App from './components/App.vue';
+
+const app = new Vue ({
+    el: '#app',
+    components: {
+        App
+    }
+});
+
+import './css/style.css'
